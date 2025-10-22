c36e719ec8d5c95c2cbe09ae06974569a6c4b3e7
Toni Mäki-Leppilampi <basstony@icloud.com>
2025-10-22 08:44:57 +0300

    path problems

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/rubik_oll_finder',
+  base: '/rubik_oll_finder/',
   plugins: [react()],
 })
